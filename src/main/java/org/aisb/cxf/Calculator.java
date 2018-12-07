e4444d767cdda165a4d0e082e2ca41a895d958bd
Doug DeJulio <ddj@cmu.edu>
2018-12-06 19:31:24 -0500

    Added some javadoc.

--- src/main/java/org/aisb/cxf/Calculator.java
+++ src/main/java/org/aisb/cxf/Calculator.java
@@ -2,8 +2,26 @@ package org.aisb.cxf;
 
 import javax.jws.*;
 
+	/**
+	 *
+	 * Do some math.
+	 *
+	 * @author Doug DeJulio
+	 *
+	 */
+	 
 @WebService
 public interface Calculator {
+
+	/**
+	 *
+	 * Add the two operands.
+	 *
+	 * @param firstOperand The first operand.
+	 * @param secondOperand The... come on, you get it.
+	 *
+	 * @return The sum.
+	 */
 
     double add(
     @WebParam(name="firstOperand")
@@ -12,6 +30,16 @@ public interface Calculator {
     double secondOperand
     );
     
+	/**
+	 *
+	 * Subtract the second operand from the first.
+	 *
+	 * @param firstOperand The first operand.
+	 * @param secondOperand Another operand.
+	 *
+	 * @return The difference.
+	 */
+
     double subtract(
     @WebParam(name="firstOperand")
     double firstOperand,
